49ad619cac54ef17a803d2904d02c2a68f288343
potatoooo <eliot.cnow@gmail.com>
2026-09-21 10:22:18 -0400

    Finished!
    Now automatically detects if item is an integer or string, and sorts them.
    Also changed the keyword for sorting from "done" to "sort", and added a variable for easily changing both the sorting keyword and the reset keyword.
    :]

--- Project.py
+++ Project.py
@@ -3,46 +3,46 @@ import os
 def clear_terminal():
     os.system("cls" if os.name == "nt" else "clear")
 
-clear_terminal() # clear terminal as it starts
+def can_be_int(string):
+    try:
+        int(string)
+    except ValueError:
+        return False # return False if error occurs (can't be converted to int)
+    else:
+        return True # return True if conversion to int works
+
+clear_terminal()        # clear terminal as it starts
 list = []
-string = False # To know if it needs to convert to integer/string (False = Number, True = String)
-selected = False # If type has already been selected
+reset_keyword = "reset" # keyword used to reset program
+sort_keyword = "sort"   # keyword used to sort the current list
 list.clear()
 
 #========================= select numbers or string =========================#
 while True:
-    type = input("Would you like to sort numbers or words/letters?\nType **numbers** or **words**\n")
-    if type == "numbers":
-        string = False
-        break
-    if type == "words":
-        string = True
-        break
-    else:
-        clear_terminal()
-        print("==========Invalid input==========")
-while True:
-    #===========================================================================#
-    item = input("Enter a word/number to add to the list: ") # store input in "item"
+    item = input("Enter a string/integer to add to the list: ") # store input in "item"
     
-    if item == "reset":
+    if item == reset_keyword:
         clear_terminal()
         list.clear()
   
-    if item == "done":
+    if item == sort_keyword:
         clear_terminal()
-        list.sort()
+        list.sort(key=lambda value: (    # sorts integers and strings seperatly since normal sort() can't do both at the same time
+            isinstance(value, str),      # check if it is a string
+            value.lower() if isinstance(value, str) else value
+        ))
         print("Sorted list: ", list)
-        print("Enter **reset** to start over")
+        print(f"Enter **{reset_keyword}** to start over")
 
     else:    
         clear_terminal()
-        if string == False:
-            item = int(item) # convert to integer if not a string
-        list.append(item) # add input stored in "item" to the list
+        if can_be_int(item):            # if item can be converted to int
+            item = int(item)            # convert to integer
+        if item != reset_keyword:       # stops the reset command to be added to the list
+            list.append(item)           # add input stored in "item" to the list
         print("Current list: ", list)
-        print("Enter **reset** to start over")
-        print("Enter **done** to finish")
-        
-        
-    
+        print(f"Enter **{reset_keyword}** to start over")
+        print(f"Enter **{sort_keyword}** to sort")
+
+
+
